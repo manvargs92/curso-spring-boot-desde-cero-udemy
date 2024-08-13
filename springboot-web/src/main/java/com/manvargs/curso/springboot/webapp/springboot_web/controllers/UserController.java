370e3bed34669a8799c0b2453d2208e08de6f15f
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-13 17:35:20 -0600

    Pasar datos a la vista con Thymeleaf.

--- springboot-web/src/main/java/com/manvargs/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/manvargs/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -3,13 +3,18 @@ package com.manvargs.curso.springboot.webapp.springboot_web.controllers;
 /* Un controlador es una clase común que maneja métodos del request conocidos como "Métodos Handler" */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // indica a Spring que es una clase controladora
 public class UserController {
 
     @GetMapping("/details") // indica que es una petición GET
-    public String details() {
+    public String details(Model model) { // Model se utiliza para pasarle datos a la vista
+        model.addAttribute("title", "Hola, Mundo con Spring Boot!!");
+        model.addAttribute("name", "Manuel");
+        model.addAttribute("lastName", "Vargas");
+
         return "details"; // si trabajamos con el modelo MVC los controladores deben retornar vistas o plantillas HTML del lado del servidor
     }
 
