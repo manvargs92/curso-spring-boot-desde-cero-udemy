9a2ee45dff10d58dcd707183bb3904d28e12155a
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-14 13:57:49 -0600

    Pasar un parámetro con @RequestParam.

---
+++ springboot-web/src/main/java/com/manvargs/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,27 @@
+package com.manvargs.curso.springboot.webapp.springboot_web.controllers;
+
+import com.manvargs.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api-params")
+public class RequestParamsController {
+
+    @GetMapping("/send-parameters")
+    public ParamDto getParams(
+            @RequestParam( // @RequestParam pasar parámetros a través de la ruta url con url?parámetro
+                    required = false, // setear si el parámetro es requerido o no
+                    defaultValue = "Sin mensaje.", // setear un valor (mensaje) por defecto
+                    name = "Mensajito") String message) { // setear el nombre del parámetro
+        ParamDto paramDto = new ParamDto();
+
+        paramDto.setMessage(message);
+//        paramDto.setMessage(message != null ? message : "Sin mensaje."); // setear un mensaje por default
+
+        return paramDto;
+    }
+
+}
