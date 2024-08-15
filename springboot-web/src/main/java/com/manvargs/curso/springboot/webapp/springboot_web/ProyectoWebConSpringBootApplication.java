4509ba20bb3d4ebfd165e3243d251445acfedc0d
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-15 12:58:09 -0600

    Agregar otros archivos properties personalizados para las configuraciones.

--- springboot-web/src/main/java/com/manvargs/curso/springboot/webapp/springboot_web/ProyectoWebConSpringBootApplication.java
+++ springboot-web/src/main/java/com/manvargs/curso/springboot/webapp/springboot_web/ProyectoWebConSpringBootApplication.java
@@ -2,8 +2,13 @@ package com.manvargs.curso.springboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ // indica a Spring que se agregarán varios archivos de configuración
+	@PropertySource("classpath:values.properties") // agregar un archivo a Spring de configuraciones externas
+})
 public class ProyectoWebConSpringBootApplication {
 
 	public static void main(String[] args) {
