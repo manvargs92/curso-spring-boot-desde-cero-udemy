f7a95b50f8a4331523b6fb10b951f931e10c7191
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-19 14:53:33 -0600

    Inmutabilidad usando la interface Cloneable.

--- springboot-inyecdepen/src/main/java/com/manvargs/curso/springboot/inyecdepen/services/ProductService.java
+++ springboot-inyecdepen/src/main/java/com/manvargs/curso/springboot/inyecdepen/services/ProductService.java
@@ -15,7 +15,9 @@ public class ProductService {
     public List<Product> findAll() {
         return repository.findAll().stream().map(product -> { // el .map generará una nueva instancia de List<Product> cada vez que se ejecute
             Double priceWithTax = product.getPrice() * 1.25; // se realiza el cálculo del precio con impuesto
-            Product newProd = new Product(product.getId(), product.getName(), priceWithTax.longValue()); // se crea un nuevo objeto de producto para no mutar los datos
+            Product newProd = (Product) product.clone(); // en vez de crear la instancia de Producto, lo clonamos
+            newProd.setPrice(priceWithTax.longValue()); // modifica el precio al Producto clonado
+//            Product newProd = new Product(product.getId(), product.getName(), priceWithTax.longValue()); // se crea un nuevo objeto de producto para no mutar los datos
 //            product.setPrice(priceWithTax.longValue()); // se modifica el precio, .longValue() - convierte el tipo del valor a Long
 
 //            return product;
