7fce0afdb6c7bccdd48367b7781d3733bb9674c7
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-19 12:32:56 -0600

    Creación de un proyecto nuevo.

---
+++ springboot-inyecdepen/src/main/java/com/manvargs/curso/springboot/inyecdepen/InyecccionDeDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.manvargs.curso.springboot.inyecdepen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyecccionDeDependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyecccionDeDependenciasApplication.class, args);
+	}
+
+}
---
+++ springboot-inyecdepen/src/test/java/com/manvargs/curso/springboot/inyecdepen/InyecccionDeDependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manvargs.curso.springboot.inyecdepen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyecccionDeDependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
