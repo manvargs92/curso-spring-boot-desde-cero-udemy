f769d7c59d596cfd51c461d98806ec02a62bf3e3
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-22 11:27:52 -0600

    Leer los datos de un archivo JSON: Otra variante para leer el JSON si la la clase estubiera anotada con @Bean.

--- springboot-inyecdepen/src/main/java/com/manvargs/curso/springboot/inyecdepen/repositories/ProductRepositoryJson.java
+++ springboot-inyecdepen/src/main/java/com/manvargs/curso/springboot/inyecdepen/repositories/ProductRepositoryJson.java
@@ -2,6 +2,7 @@ package com.manvargs.curso.springboot.inyecdepen.repositories;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manvargs.curso.springboot.inyecdepen.models.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -13,11 +14,15 @@ public class ProductRepositoryJson implements IProductRepository {
 
     private List<Product> list;
 
+//    @Value("classpath:json/product.json")
+//    private Resource resource; // leer el JSON inyectándolo con @Value (forma declarativa) (solo fucniona si la clase es un componente Spring)
+
     public ProductRepositoryJson() {
-        Resource resource = new ClassPathResource("json/product.json"); // leer un archivo .json
+        Resource resource = new ClassPathResource("json/product.json"); // leer un archivo .json (forma programática)
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class)); // convertir el json en un arreglo
+            list = Arrays.asList(objectMapper.readValue(resource.getInputStream(), Product[].class)); // convertir el json en un arreglo
+//            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class)); // convertir el json en un arreglo
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
