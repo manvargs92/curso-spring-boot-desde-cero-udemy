a44a96d177ae00cddf2d4896b7854f657c3f9a83
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-25 13:27:03 -0600

    Factura (Invoice) y relaciones entre sus componentes.

--- springboot-difactura/src/main/java/com/manvargs/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ springboot-difactura/src/main/java/com/manvargs/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -1,5 +1,41 @@
 package com.manvargs.curso.springboot.di.factura.springboot_difactura.models;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Invoice {
+
+    private String description;
+    private List<Item> items;
+
+    @Autowired
+    private Client client;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 
 }
--- springboot-difactura/src/main/java/com/manvargs/curso/springboot/di/factura/springboot_difactura/models/Item.java
+++ springboot-difactura/src/main/java/com/manvargs/curso/springboot/di/factura/springboot_difactura/models/Item.java
@@ -2,4 +2,24 @@ package com.manvargs.curso.springboot.di.factura.springboot_difactura.models;
 
 public class Item {
 
+    private int Quantity;
+
+    private Product product;
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
 }
