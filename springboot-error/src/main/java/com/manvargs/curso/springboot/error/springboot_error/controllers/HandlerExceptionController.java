c10ff680062f9f99a8219c0f88f14ee1025d0565
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-09-01 00:13:18 -0600

    Personalizando página de error para la excepción NumberFormatException.

--- springboot-error/src/main/java/com/manvargs/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ springboot-error/src/main/java/com/manvargs/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.manvargs.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -42,4 +45,19 @@ public class HandlerExceptionController {
         // return ResponseEntity.notFound().build(); // si no quiero personalizar el body de respuesta
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
+
+    // otraalternativa para formar los métodos con un Map<>
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatEx(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+
+        error.put("date", new Date());
+        error.put("error", "Número inválido o incorrecto, no tiene formato de dígito.");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
+    }
+
 }
