1068cce381e4528b68387f713b2c48740a9210d0
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-09-02 12:15:56 -0600

    Creando nuevo proyecto - Interceptores Http.

---
+++ springboot-interceptors/src/main/java/com/manvargs/curso/springboot/app/interceptors/SpringbootInterceptorsApplication.java
@@ -0,0 +1,13 @@
+package com.manvargs.curso.springboot.app.interceptors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterceptorsApplication.class, args);
+	}
+
+}
---
+++ springboot-interceptors/src/test/java/com/manvargs/curso/springboot/app/interceptors/SpringbootInterceptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manvargs.curso.springboot.app.interceptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
